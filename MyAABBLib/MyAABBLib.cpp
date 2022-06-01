c043ea7030c01221776912912a4b4e11ce762d1b
Tygo <tygoboons@home.nl>
2022-06-01 19:58:21 +0200

    Initial commit

---
+++ MyAABBLib/MyAABBLib.cpp
@@ -0,0 +1,12 @@
+﻿// MyAABBLib.cpp : Defines the entry point for the application.
+//
+
+#include "MyAABBLib.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
