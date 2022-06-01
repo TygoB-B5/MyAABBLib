386dc7c90f7f48edf72213981a29079d8e6c3ed3
Tygo <tygoboons@home.nl>
2022-06-01 23:40:03 +0200

    Initialized SFML

--- MyAABBLib/MyAABBLib.cpp
+++ MyAABBLib/MyAABBLib.cpp
@@ -1,12 +1,24 @@
-﻿// MyAABBLib.cpp : Defines the entry point for the application.
-//
-
-#include "MyAABBLib.h"
-
-using namespace std;
+﻿#include <SFML/Graphics.hpp>
 
 int main()
 {
-	cout << "Hello CMake." << endl;
-	return 0;
+    sf::RenderWindow window(sf::VideoMode(sf::Vector2u(200, 200)), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
 }
